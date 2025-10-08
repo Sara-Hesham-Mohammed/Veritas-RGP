de8b1d68a7198593db2afe6e9e047694969aa5f7
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2025-10-08 17:19:53 +0300

    v1.0

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import pandas as pd
+import glob
 
-'''
-@:param file_paths: List of file paths to Excel files
-@:return: Final DataFrame containing data from all Excel files
-'''
+# folder path
+folder_path = "E:\Veritas Debate League\Registration"
 
-# files path
-file_paths = []
+def merge_excel_files(end_path):
+    """
+    @param end_path: List of file paths to Excel files
+    @return: Final DataFrame containing data from all Excel files
+    """
 
-# loop through each and load them into a dataframe
-for file in file_paths:
-    # Read the Excel file into a DataFrame
-    df = pd.read_excel(file)
-    print(df.head()) # Display the first few rows
+    excel_files = glob.glob(f"{folder_path}/{end_path}/*.xlsx")
+
+    dataframes = [pd.read_excel(file) for file in excel_files]
+    for df in dataframes:
+        # Convert column to numeric type
+        df['Speaker 1 Phone'] = df['Speaker 1 Phone'].astype('str')
+        df['Speaker 2 Phone'] = df['Speaker 2 Phone'].astype('str')
+        df['Speaker 3 Phone'] = df['Speaker 3 Phone'].astype('str')
+        df.drop_duplicates(keep="last")
+        df.astype(str)
+        df.info()
+        print(df.dtypes)
+
+    final_df = pd.concat(dataframes, ignore_index=True).astype(str)
+
+    final_df.to_excel(f"E:/Veritas Debate League/Registration/{end_path}.xlsx", index=False)
+    print(final_df.head())  # Display the first few rows
+    print(final_df.columns)  # Display the first few rows
+    return final_df
+
+
+merge_excel_files("High School")
+# merge_excel_files("Middle School")
