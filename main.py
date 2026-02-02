51428c38e7c7facbcf80c1949e8880cde120334e
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2026-02-03 00:17:27 +0200

    removed the numbers to string safety check

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import glob
 import duckdb
 
 # folder path
-folder_path = "E:/Veritas Debate League/Registration/Nov. 29"
+folder_path = "E:/Veritas Debate League/Registration/Feb. 7"
+
+def validity_check(df):
+    if df[df['col'].isna()]:
+        pass
+
+
 
 def merge_excel_files(end_path):
     """
@@ -16,21 +22,17 @@ def merge_excel_files(end_path):
     dataframes = []
     for file in excel_files:
         try:
-            df = pd.read_excel(file)
+            df = pd.read_excel(file,  sheet_name='Template')
             dataframes.append(df)
         except Exception as e:
             print(f"Could not read {file.title}: {e}")
 
     for df in dataframes:
         try:
-            # Convert column to numeric type
-            df['Speaker 1 Phone'] = df['Speaker 1 Phone'].astype('str')
-            df['Speaker 2 Phone'] = df['Speaker 2 Phone'].astype('str')
-            df['Speaker 3 Phone'] = df['Speaker 3 Phone'].astype('str')
-            df.drop_duplicates(keep="last")
+            # Convert all columns to string (modify original)
             df.astype(str)
         except Exception as e:
-            print(f" Exception in one of the {df.keys()}: {e}")
+            print(f"Exception in dataframe with columns {list(df.columns)}: {e}")
 
     final_df = pd.concat(dataframes, ignore_index=True).astype(str)
 
